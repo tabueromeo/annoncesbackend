d99ea5862ed2ca4f5553db6ba99359a7049abde6
tabue romeo <tabueromeo@gmail.com>
2023-02-15 11:43:46 +0100

    update message racine get

--- index.js
+++ index.js
@@ -1,62 +1,74 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const config = require('./config/config');
 const app = express();
 var bodyParser = require('body-parser');
 var router = express.Router();
-const mongoose = require("mongoose");
-const https = require("https");
+const mongoose = require('mongoose');
+const https = require('https');
 const fs = require('fs');
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-if(config.statusDev=="dev"){
-  mongoose.connect("mongodb+srv://lovons:lovons@lovons.jwtjz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }).then(() => {
-    console.log('Connected to mongoDB')
-  }).catch(e => {
-    console.log('Error while DB connecting');
-    console.log(e);
-  });
-}else{
-  mongoose.connect(config.CURRENT_BD_PATH).then(() => {
-    console.log('Connected to mongoDB')
-  }).catch(e => {
-    console.log('Error while DB connecting');
-    console.log(e);
-  });
+if (config.statusDev == 'dev') {
+  mongoose
+    .connect(
+      'mongodb+srv://lovons:lovons@lovons.jwtjz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      console.log('Connected to mongoDB');
+    })
+    .catch((e) => {
+      console.log('Error while DB connecting');
+      console.log(e);
+    });
+} else {
+  mongoose
+    .connect(config.CURRENT_BD_PATH)
+    .then(() => {
+      console.log('Connected to mongoDB');
+    })
+    .catch((e) => {
+      console.log('Error while DB connecting');
+      console.log(e);
+    });
 }
 
-
-
-
-
-
-  //Définition des CORS
+//Définition des CORS
 app.use(function (req, res, next) {
   res.setHeader('Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type,Authorization'
+  );
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.send(" debut du boulot."))
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.send(' Backend site annonce setup.'));
 
-  //On définit notre objet express nommé app
-app.use(bodyParser.urlencoded({parameterLimit: 10000, limit: '50mb', extended: true }));
-app.use(bodyParser.json({parameterLimit: 10000, limit: '50mb'}));
+//On définit notre objet express nommé app
+app.use(
+  bodyParser.urlencoded({
+    parameterLimit: 10000,
+    limit: '50mb',
+    extended: true,
+  })
+);
+app.use(bodyParser.json({ parameterLimit: 10000, limit: '50mb' }));
 
-
-
-
-
-
-// appel des routes 
+// appel des routes
 
 app.use('/category', router);
 require('./controllers/CategoryControllers')(router);
@@ -70,18 +82,18 @@ require('./controllers/PrivilegeController')(router);
 app.use('/user', router);
 require('./controllers/userController')(router);
 
-if(config.statusDev=="dev"){
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-}else{
+if (config.statusDev == 'dev') {
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+} else {
   const options = {
-    key: fs.readFileSync("/etc/letsencrypt/live/back.lovons.com/privkey.pem"),
-           cert: fs.readFileSync("/etc/letsencrypt/live/back.lovons.com/fullchain.pem"),
-           ca: fs.readFileSync("/etc/letsencrypt/live/back.lovons.com/chain.pem")
-   };
-   
-   https.createServer(options,app).listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-  
+    key: fs.readFileSync('/etc/letsencrypt/live/back.lovons.com/privkey.pem'),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/back.lovons.com/fullchain.pem'
+    ),
+    ca: fs.readFileSync('/etc/letsencrypt/live/back.lovons.com/chain.pem'),
+  };
+
+  https
+    .createServer(options, app)
+    .listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 }
-
-
-
